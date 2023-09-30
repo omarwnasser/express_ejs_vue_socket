24553fb75438e835455180245270c09c1b8d0d89
omarwnasser <82316429+omarwnasser@users.noreply.github.com>
2023-09-30 10:35:29 +0300

    express scoket.io vue ejs

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { Server } = require("http");
+const cors = require("cors");
+const io = require("socket.io");
+
+const app = express();
+
+app.use(cors());
+app.set("view engine", "ejs");
+
+const server = new Server(app);
+
+const socket = new io.Server(server);
+
+socket.on("connection", (so) => {
+  console.log(so.id);
+
+  so.on('send ready request',(data)=>{
+    socket.emit('add ready request',data)
+  })
+
+  so.on('get all list',()=>{
+    console.log('get all list');
+    socket.emit('send all list')
+  })
+
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+app.get("/**", function (req, res) {
+  res.send("page not found");
+});
+
+server.listen(3000, () => {
+  console.log("connect to server successfully");
+});
